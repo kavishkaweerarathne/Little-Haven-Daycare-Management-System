feb578561b86d32994cdeee475384036dc07577f
Kavishka Weerarathne <kdweerarathne@gmail.com>
2026-05-16 11:01:06 +0530

    Update logout.php

--- login/logout.php
+++ login/logout.php
@@ -1,7 +1,7 @@
 <?php
-session_start();
-session_unset();
-session_destroy();
-header("Location: login.php");
-exit();
+session_start(); // start session
+session_unset(); // unset all session variables
+session_destroy(); // destroy session
+header("Location: login.php"); // redirect to login page
+exit(); // exit
 ?>
